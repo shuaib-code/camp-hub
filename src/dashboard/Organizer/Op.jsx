e8f01ffa2354f823b9af699a4d8f6e64ea7a08c1
shuaib-cyclist <shuaib.cyclist@gmail.com>
2023-11-27 18:58:32 +0600

    Organizer and participant profile card

--- src/dashboard/Organizer/Op.jsx
+++ src/dashboard/Organizer/Op.jsx
@@ -21,13 +21,50 @@ const Op = () => {
       </div>
     );
   }
+  let data = profile?.data.length ? profile.data[0] : [];
   return (
     <div className="w-full">
-      <h1>Proile organizer</h1>
-      <OpModal
-        profile={profile?.data?.length ? profile?.data[0] : []}
-        refetch={profile.refetch}
-      ></OpModal>
+      <h1 className="form-title">Organizer Profile</h1>
+      <div className="w-full px-4 mt-5">
+        <div className="block text-sm text-gray-500 bg-white border border-gray-200 rounded">
+          <div className="p-3">
+            <div className="flex items-center justify-between mb-2">
+              <img
+                className="w-10 h-10 rounded-full"
+                src={user.photoURL}
+                referrerPolicy="false"
+              />
+              <div>
+                <OpModal
+                  profile={profile?.data?.length ? profile?.data[0] : []}
+                  refetch={profile.refetch}
+                ></OpModal>
+              </div>
+            </div>
+            <p className="text-base font-semibold leading-none text-gray-900 dark:text-white">
+              {user.displayName}
+            </p>
+            <p className="mb-3 text-sm font-normal">{user.email}</p>
+            <p className="text-sm">
+              <span className="text-gray-700 font-medium">Twetter:</span>{" "}
+              {data?.twitter || "not fouond"}
+            </p>
+            <p className="text-sm">
+              <span className="text-gray-700 font-medium">Phone:</span>{" "}
+              {data?.phone || "not fouond"}
+            </p>
+            <p className="mb-3 text-sm">
+              <span className="text-gray-700 font-medium">Linkedin:</span>{" "}
+              {data?.linkedin || "not fouond"}
+            </p>
+            <p className="mb-3 text-sm">
+              <span className="text-gray-700 font-medium">Success Story:</span>{" "}
+              {data?.story || "not fouond"}
+            </p>
+          </div>
+          <div data-popper-arrow></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/dashboard/Participant/Pp.jsx
+++ src/dashboard/Participant/Pp.jsx
@@ -21,13 +21,46 @@ const Pp = () => {
       </div>
     );
   }
+  let data = profile?.data.length ? profile.data[0] : [];
   return (
     <div className="w-full">
-      <h1>Proile organizer</h1>
-      <Pmodal
-        profile={profile?.data?.length ? profile?.data[0] : []}
-        refetch={profile.refetch}
-      ></Pmodal>
+      <h1 className="form-title">Participant Profile</h1>
+      <div className="w-full px-4 mt-5">
+        <div className="block text-sm text-gray-500 bg-white border border-gray-200 rounded">
+          <div className="p-3">
+            <div className="flex items-center justify-between mb-2">
+              <img
+                className="w-10 h-10 rounded-full"
+                src={user.photoURL}
+                referrerPolicy="false"
+              />
+              <div>
+                <Pmodal
+                  profile={profile?.data?.length ? profile?.data[0] : []}
+                  refetch={profile.refetch}
+                ></Pmodal>
+              </div>
+            </div>
+            <p className="text-base font-semibold leading-none text-gray-900 dark:text-white">
+              {user.displayName}
+            </p>
+            <p className="mb-3 text-sm font-normal">{user.email}</p>
+            <p className="text-sm">
+              <span className="text-gray-700 font-medium">Twetter:</span>{" "}
+              {data?.twitter || "not fouond"}
+            </p>
+            <p className="text-sm">
+              <span className="text-gray-700 font-medium">Phone:</span>{" "}
+              {data?.phone || "not fouond"}
+            </p>
+            <p className="mb-3 text-sm">
+              <span className="text-gray-700 font-medium">Linkedin:</span>{" "}
+              {data?.linkedin || "not fouond"}
+            </p>
+          </div>
+          <div data-popper-arrow></div>
+        </div>
+      </div>
     </div>
   );
 };
